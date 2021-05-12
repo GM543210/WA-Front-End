a78d4dc2a9965dbe7b6ec4e9c89bce4387a06950
Gabriel <gmikulaco@unipu.hr>
2021-05-12 19:34:17 +0200

    Small tests and base images addition

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,11 @@
 <template>
-  <div id="app">
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
-      <router-link class="navbar-brand" to="/">Our App</router-link>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li>Todo stranica</li>
-        </ul>
-        <router-link v-if="!authenticated" class="btn btn-info my-2 my-sm-0 mr-2" to="/login">Login</router-link>
-        <span v-if="authenticated">
-          {{ userName }}
-          <a @click="logout" class="btn btn-info my-2 my-sm-0 mr-2" href="#">Logout</a>
-        </span>
-        <router-link class="btn btn-outline my-2 my-sm-0 mr-2" to="/signup">Signup</router-link>
-      </div>
-    </nav>
-
     <router-view/>
-
-    <div id="footer" class="text-center mt-5">Ovo je footer. (c) 2019 mi svi skupa.</div>
-  </div>
 </template>
 
 <script>
 import store from '@/store.js'
+import router from '@/router';
+// import { firebase } from '@/firebase';
 
 export default {
   data () {
