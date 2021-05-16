faf2c99ff0b5b6c44f7ea142fb9f6d23e636f9aa
Gabriel <gmikulaco@unipu.hr>
2021-05-16 17:02:27 +0200

    Added dormant methods, Institutions.vue component

---
+++ src/components/Institutions.vue
@@ -0,0 +1,60 @@
+<template>
+
+<div class="container centered">
+    <div class="row">
+        <div class="col-2">
+            <div class="institutions" @click="onInstitutionSelected()">
+                <img class="resize" src="@/assets/institution.png">
+                <span class="caption">{{ institutions.Caption }}</span>
+                <span class="caption">{{ institutions.Adress }}{{ institutions.City }}</span>
+                <span class="caption">{{ institutions.WorkingHours }}</span>
+            </div>
+        </div>
+        <br>
+
+    </div>
+</div>
+
+</template>
+
+<script>
+export default {
+    name: 'Institutions',
+    props: ['institution',],
+    methods: {
+        onInstitutionSelected() { // unknown method god knows what this does
+            this.$emit('institution-selected', this.institution);
+        },
+    },
+};
+
+</script>
+
+<style>
+
+.institutions {
+    cursor: pointer;
+}
+
+.resize {
+    width: 5%;
+}
+
+.products {
+    display: inline-block;
+    margin-left: 100px;
+}
+
+.category-filter {
+    vertical-align: top;
+    display: inline-block;
+    text-align: center;
+    margin-right: 3%;
+}
+
+.caption {
+    display: block;
+    margin-top: 2px;
+    width: 100px;
+}
+</style>
--- src/views/Main-User.vue
+++ src/views/Main-User.vue
@@ -7,6 +7,14 @@
     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
   </div>
+  <!-- 
+    <div class="col-4 PrListing">
+       <div class="centered scroll">
+        <Institutions v-for="institution in INST" :key="institution.Caption" :institution="institution" @product-selected="setSelectedProduct" />
+       </div>
+        <button type="button" class="button addbtn showBtn" @click="showMore"><span>Show more institutions</span></button>
+     </div>
+   -->
   <Footer />
  </div>
 </template>
@@ -15,6 +23,9 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
+import Institutions from '@/components/Institutions.vue';
+import store from '@/store';
+import { firebase } from '@/firebase';
 
 export default {
   name: 'main-user',
@@ -22,6 +33,44 @@ export default {
     HelloWorld,
     Header,
     Footer
+  },
+  data: function() {
+      return {
+            store,
+            Institutions,
+            imageReference1: null,
+            imageReference2: null,
+            institution_name: '',
+            branch_office_city: '',
+            institution_adress: '',
+            institution_wh: '',
+            INST:[],
+            
+      }
+  },
+  mounted(){
+    this.getIntitutions();
+  },
+  methods:{
+    getIntitutions() { //sluzit ce za Listing Ustanova/Institucija
+            firebase.firestore()
+            .collection('INSTITUTIONS')
+            .get()
+            .then((query) => {
+                query.forEach((doc) => {
+
+                    const data = doc.data();
+                        this.INST.push({
+                            'Caption': data.Name,
+                            'City': data.InstitutionBranchCity,
+                            'Adress' : data.InstitutionAdress,
+                            // 'Availabilitydate': data.Availabilitydate,
+                            'WorkingHours': data.InstitutionWH,
+                        })
+                    console.log(data)
+                });
+            });
+        }
   }
 }
 </script>
